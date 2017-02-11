f0df2140545073d09828c5c64b4b6b0b3755f819
new2scala <jiajiwu.icare@gmail.com>
2017-02-11 02:45:11 +0100

    t

---
+++ dsfs/ch5-stats.py
@@ -0,0 +1,41 @@
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+num_friends = [100,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+friend_counts = Counter(num_friends)
+
+'''
+max_x = 101
+xs = range(max_x)
+ys = [friend_counts[x] for x in xs]
+
+plt.bar(xs, ys)
+plt.axis([0, max_x, 0, 10])
+
+plt.show()
+'''
+
+num_points = len(num_friends)
+
+largest_val = max(num_friends)
+smallest_val = min(num_friends)
+
+def mean(x):
+    return sum(x) / len(x)
+
+print(mean(num_friends))
+
+def median(v):
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+    if n % 2 == 1:
+        return sorted_v[midpoint]
+    else:
+        l = midpoint-1
+        h = midpoint
+        return (sorted_v[l]+sorted_v[h])/2
+
+print(median(num_friends))
